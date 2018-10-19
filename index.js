ca8186fc174b4bf13b132aa4fc92fc77237bcc1e
rlcoley <rosscoley1002@gmail.com>
2018-10-18 20:08:00 -0400

    added winner function that is called when each letter of the word is guessed. set text of hint to not show when new game button is pressed

--- index.js
+++ index.js
@@ -8,31 +8,46 @@ var newGame = document.getElementById('newgame')
 var submitGuess = document.getElementById("clear")
 var input = document.getElementById('input')
 var wrong = document.getElementById('wrong')
+var wordBox = document.getElementById('wordBox')
 
 var randomWord;
 var theWord;
-var theHint;
+var theHint = "";
+var counter;
 var guess = []
 var usedLetters = []
 
 var words = [w1,w2,w3,w4]
 
 
-function showMysteryWord() {
+function startGame() {
+  while (wordBox.firstChild){
+    wordBox.removeChild(wordBox.firstChild);
+  }
+
+  if (showHint != ""){
+    giveHint()
+  }
   randomWord = Math.floor(Math.random()* words.length);
   theWord = words[randomWord].word.split('')
+  counter = theWord.length;
   theHint = words[randomWord].hint
   console.log(theWord);
   console.log(theHint);
+  console.log(counter);
 
   for (var i = 0; i < theWord.length; i++) {
+
     var boxes = document.createElement('div');
     boxes.className = 'boxes'
     boxes.id = 'box'+ i;
-    document.body.appendChild(boxes);
+    wordBox.appendChild(boxes);
     if (theWord[i] == ' ') {
+      counter--;
+      console.log(counter);
       boxes.style.border = 'none';
       guess.push(' ')
+      theWord[i] ='';
     }else {
       guess.push('-')
     }
@@ -43,45 +58,58 @@ function showMysteryWord() {
 
 // clear input section when Guess btn is clicked //
 // function clearInput() {
-submitGuess.addEventListener('click',function() {
-  // console.log(input.value);
-  // console.log(inputArea.keypress);
-  // input.value = "";
-  // checkGuess()
-})
+document.addEventListener('keypress', (event) => {
+  if(event.keyCode === 13 && input.value!=""){
+    checkGuess()
+  }
+});
 
 submitGuess.addEventListener('click',checkGuess)
 
 
 function checkGuess() {
+  var inTheWord;
   for (var i = 0; i < theWord.length; i++) {
+     inTheWord = document.getElementById('box'+[i])
     // check if game is done then, check later then check if they //
     // if any box id .innerHTML = " ". game not done loop through //
     if (theWord[i].toLowerCase() == input.value.toLowerCase() ) {
-      document.getElementById('box'+[i]).innerHTML = theWord[i]
+      inTheWord.innerHTML = theWord[i]
+      counter--
+      console.log(counter);
+      // inTheWord.style.marginLeft = ""
+      inTheWord.style.backgroundColor = 'orange'
+
     }
   }
   usedLetters.push(input.value);
   wrong.innerHTML = usedLetters.join(", ");
+  if (counter == 0) {
+    winner();
+  }
   input.value = "";
 }
 
+function winner() {
+  // setTimeout(function(){alert("You won!! Don't be too happy cause you still suck");},1000)
+  setTimeout(function(){if (window.confirm("You Won, but still suck! Wanna Play Again?")){startGame();}}, 1000)
+  // startGame();
+}
 
-// function clear() {
-//
-// }
-
+function giveHint() {
+  if(showHint.innerHTML != theHint){
+    showHint.innerHTML = theHint;
+  } else {
+    showHint.innerHTML = "";
+  }
+}
 
 newGame.addEventListener('click',function() {
-  showMysteryWord()
-  // clear()
+  startGame()
+  showHint.innerHTML = "";
 
 })
 
-function giveHint() {
-  showHint.innerHTML = theHint;
-}
-
 hintButton.addEventListener('click', function() {
-  giveHint()
+  giveHint();
 })
